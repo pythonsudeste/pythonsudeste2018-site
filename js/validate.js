b1dff518f8eff930e57ec46c62d59a0822b517b9
Danilo J. S. Bellini <danilo.bellini@gmail.com>
2018-04-23 01:18:58 -0300

    Insere a lógica de validação!
    Os hashes dos certificados são reconstruídos em JavaScript e
    validados, verificando se eles pertencem à lista de hashes válidos

---
+++ js/validate.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById("certs-verify").addEventListener("click", function(){
+    var code = getCode();
+
+    if(code.length !== 19){
+      showResult([
+        "ERRO!", "",
+        "Código com tamanho inválido!", "",
+        "Os códigos são da forma XXXX-XXXX-XXXX-XXXX."
+      ].join("<br>"), "#ffaaaa");
+      return;
+    };
+
+    var category = document.getElementById("certs-category").value;
+    var name = getName();
+    var msg = buildMessage(category, code, name);
+    var hash = sha3_224(msg);
+
+    if(!hashes.includes(hash)){
+      showResult([
+        "Oops!", "",
+        [
+          "<ul>",
+            "<li>Ou esse certificado é inválido;",
+            "<li>Ou há algum campo que não foi preenchido corretamente.",
+          "</ul>"
+        ].join(""),
+      ].join("<br>"), "#ffaaaa");
+    } else showResult("Certificado válido!", "#aaffaa");
+  });
+});
+
+function lowerNormalize(name){
+  return name.normalize("NFD")
+             .replace(/[\u0300-\u036f]/g, "")
+             .replace(/\s+/g, " ")
+             .trim()
+             .toLowerCase();
+};
+
+function getName(){
+  return lowerNormalize(document.getElementById("certs-name").value);
+};
+
+function getCode(){
+  return document.getElementById("certs-code")
+                 .value
+                 .replace(/\s|-/g, "")
+                 .replace(/(....)(....)(....)(....)/g, "$1-$2-$3-$4");
+};
+
+function buildMessage(category, code, name){
+  return `2018/Python-${category}_${code}_${name}-Sudeste`;
+};
+
+function showResult(data, color){
+  var node = document.getElementById("certs-result");
+  node.innerHTML = data;
+  node.style.color = color;
+};
